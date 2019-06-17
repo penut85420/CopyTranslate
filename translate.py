a21f68c70ca69a172db809c0e1f5a284e927b94c
PenutChen <penut85420@gmail.com>
2019-06-17 09:31:02 +0800

    Update config file

--- translate.py
+++ translate.py
@@ -1,22 +1,32 @@
-from googletrans import Translator
-import pyperclip as pc
-import time
+import json
+import os
 import sys
+import time
+
+import pyperclip as pc
+from googletrans import Translator
+
 
 class TranslationHero(Translator):
     def __init__(self, dest='en'):
         Translator.__init__(self)
         self.dest = dest
-        self.get_trans('Test')
+        try:
+            self.get_trans('Test')
+        except ValueError:
+            print('Error: %s is an invalid destination language.' % self.dest)
+            exit(1)
     
     def get_trans(self, s):
         t = self.translate(s, dest=self.dest)
         return t.text
 
+
 class ClipboardMonitor:
-    def __init__(self, update_seconds=1, dest='en', no_first=False):
-        self.update_seconds = update_seconds
-        self.th = TranslationHero(dest)
+    def __init__(self, update_time=1, destination_language='en', no_first=False):
+        self.update_seconds = update_time
+        self.dest = destination_language
+        self.th = TranslationHero(destination_language)
         self.last = pc.paste() if no_first else str()
         self.now = self.last
         self.origin = str()
@@ -25,20 +35,25 @@ class ClipboardMonitor:
 
     def run(self):
         self.pause = False
+        print('[Copy Translate]')
+        print('Update time: %.1f' % self.update_seconds)
+        print('Destination language: %s' % self.th.dest.upper())
         print('Begin translating!')
         try:
             self.monitor()
         except KeyboardInterrupt:
-            print('Bye!')
+            print('\nBye!')
             exit(0)
+        except:
+            self.th = TranslationHero(self.dest)
 
     def iter(self):
         self.now = pc.paste()
         if self.now != self.last and len(self.now.strip()) != 0:
             self.origin = self.preprocessing(self.now)
-            print('Origin: %s' % self.origin)
+            print('\n[Origin]\n%s' % self.origin)
             self.trans = self.th.get_trans(self.origin)
-            print('Translation: %s' % self.trans)
+            print('[Translation]\n%s' % self.trans)
             self.last = self.now
     
     def monitor(self):
@@ -52,31 +67,73 @@ class ClipboardMonitor:
         t = t.replace('  ', ' ')
         return t.strip()
 
-if __name__ == '__main__':
-    i = 0
-    gap = 1
-    dest = 'en'
-    no_first = False
-    while i < len(sys.argv):
-        if sys.argv[i] == '-t' or sys.argv[i] == '--update-time':
-            i += 1
+
+class ConfigParser:
+    CONFIG_PATH = './copy_translate_config.ini'
+
+    def __init__(self, *args):
+        self.gap = 1
+        self.dest = 'en'
+        self.no_first = False
+        self.args = dict()
+        self.init_args()
+        self.load_config()
+        self.parse(*args)
+        self.save_config()
+    
+    def add_args(self, method, cmds):
+        for cmd in cmds:
+            self.args[cmd] = method
+
+    def init_args(self):
+        def set_gap(self, args):
             try:
-                gap = float(sys.argv[i])
+                self.gap = float(args[1])
             except:
                 print('Error: -t must follow by numeric.')
                 exit(1)
-        elif sys.argv[i] == '-d' or sys.argv[i] == '--destination-lang':
-            i += 1
-            dest = sys.argv[i]
-        elif sys.argv[i] == '-h':
+        
+        def set_dest(self, args):
+            self.dest = args[1]
+        
+        def set_no_first(self, args):
+            self.no_first = True
+        
+        def set_first(self, args):
+            self.no_first = False
+
+        def get_help(self, *args):
             print('Usage: py translate.py [-t update_time] [-d destination language] [-n]')
             exit(0)
-        elif sys.argv[i] == '-n':
-            no_first = True
-        i += 1
+        
+        self.add_args(method=set_gap, cmds=['-t', '--update-time'])
+        self.add_args(method=set_dest, cmds=['-d', '--destination-lang'])
+        self.add_args(method=set_no_first, cmds=['-n', '--no-first'])
+        self.add_args(method=set_first, cmds=['-f', '--first'])
+        self.add_args(method=get_help, cmds=['-h', '--help'])
     
-    try:
-        cm = ClipboardMonitor(update_seconds=gap, dest=dest, no_first=no_first)
-        cm.run()
-    except ValueError:
-        print('Error: %s is not a valid destination language.' % dest)
+    def parse(self, *args):
+        for i, arg in enumerate(args):
+            if self.args.get(arg, None):
+                self.args[arg](self, args[i:])
+
+    def load_config(self):
+        if os.path.exists(ConfigParser.CONFIG_PATH):
+            with open(ConfigParser.CONFIG_PATH, 'r', encoding='UTF-8') as fin:
+                self.config = json.load(fin)
+                self.gap = self.config['update_time']
+                self.dest = self.config['destination_language']
+                self.no_first = self.config['no_first']
+    
+    def save_config(self):
+        with open(ConfigParser.CONFIG_PATH, 'w', encoding='UTF-8') as fout:
+            self.config = {
+                'update_time': self.gap,
+                'destination_language': self.dest,
+                'no_first': self.no_first
+            }
+            json.dump(self.config, fout, indent=2)
+
+
+if __name__ == '__main__':
+    ClipboardMonitor(**ConfigParser(*sys.argv).config).run()
